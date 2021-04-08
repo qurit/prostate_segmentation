1b0ea8764b6c966e20fb696271ec80383e3d1ed7
rdesc <rodrigue.de.schaetzen@gmail.com>
2021-04-08 05:35:59 +0000

    clean up of env.yml and updates setup script

--- setup_workspace.sh
+++ setup_workspace.sh
@@ -23,11 +23,10 @@ echo "conda activate prostate" >> ~/.bashrc
 conda activate prostate
 
 # install detectron2 + dependencies
-pip install pyyaml==5.1 pycocotools==2.0.1 gdown
-conda install -y pytorch torchvision cudatoolkit=10.2 -c pytorch
+conda install pytorch torchvision torchaudio cudatoolkit=11.1 -c pytorch -c conda-forge
 conda install -y pandas
 conda install -y -c conda-forge opencv
-pip install detectron2 -f https://dl.fbaipublicfiles.com/detectron2/wheels/cu102/torch1.6/index.html
+python -m pip install detectron2 -f https://dl.fbaipublicfiles.com/detectron2/wheels/cu101/torch1.8/index.html
 # make sure installation contains no errors
 python -m detectron2.utils.collect_env
 

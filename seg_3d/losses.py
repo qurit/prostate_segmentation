Fixing softmax in Boundary Losses

seg_3d/losses.py
@@ -71,9 +71,7 @@ class SurfaceLoss:
 
     def __call__(self, probs: torch.Tensor, dist_maps: torch.Tensor) -> torch.Tensor:
 
-        probs = nn.Softmax(dim=1)(probs)
-
-        pc = probs.type(torch.float32)
+        pc = nn.Softmax(dim=1)(probs).type(torch.float32)
         dc = dist_maps.type(torch.float32)
 
         if self.idc:
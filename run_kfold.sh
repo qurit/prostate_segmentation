82827cd0e2b211cf18eb14f5afe9e03077dede21
rdesc <rodrigue.de.schaetzen@gmail.com>
2023-01-12 16:48:53 +0000

    comments

--- run_kfold.sh
+++ run_kfold.sh
@@ -8,6 +8,8 @@
 # i.e., TEST.FINAL_EVAL_METRICS is not empty
 # and INFERENCE_FILE_NAME is set to a filename
 
+# append '-u' to train_loop to disable sacred logging
+
 CFG=$1;
 NUM_FOLDS=$2;
 NAME="$(basename -- ${CFG%.yaml})"
@@ -22,13 +24,13 @@ for i in $(seq 1 $NUM_FOLDS)
 do 
   echo "Running $CFG for fold $COUNT...";
   # train
-  python -m seg_3d.train_loop -n $NAME with "CONFIG_FILE=$CFG" "LOAD_ONLY_CFG_FILE=True" "DATASET.FOLD=$COUNT"
+  python -m seg_3d.train_loop -n $NAME with "CONFIG_FILE=$CFG" "LOAD_ONLY_CFG_FILE=True" "DATASET.FOLD=$COUNT"  # -u
   # eval
   echo "Evaluating on test set for fold $COUNT...";
-  python -m seg_3d.train_loop -n $NAME with "CONFIG_FILE=$CFG" "LOAD_ONLY_CFG_FILE=True" "DATASET.FOLD=$COUNT" "EVAL_ONLY=True"
+  python -m seg_3d.train_loop -n $NAME with "CONFIG_FILE=$CFG" "LOAD_ONLY_CFG_FILE=True" "DATASET.FOLD=$COUNT" "EVAL_ONLY=True"  # -u
 
   let COUNT++;
 done
 
 echo "Final evaluation for $CFG..."
-python -m seg_3d.evaluation.aggregate_preds -n $NAME with "load_inference_fp='eval_0/inference.pk'"
+python -m seg_3d.evaluation.aggregate_preds -n $NAME with "load_inference_fp='eval_0/inference.pk'"  # -u
